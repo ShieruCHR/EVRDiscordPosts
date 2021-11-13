05bb0d0ce4e73544d42812fa1dfbc6f6d265fd68
Shieru <81741561+ShieruCHR@users.noreply.github.com>
2021-11-14 06:57:46 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,5 +13,3 @@ def timedelta_to_str(delta: timedelta):
     mm, ss = divmod(delta.seconds, 60)
     s = "%d分%02d秒" % (mm, ss)
     return s
-
-print(timedelta_to_str(timedelta(minutes=5, seconds=30, microseconds=120)))
