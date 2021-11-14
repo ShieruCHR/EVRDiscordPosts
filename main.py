e8253ac0d6c56cf698da6145198e698538c72a44
Shieru <81741561+ShieruCHR@users.noreply.github.com>
2021-11-15 07:55:19 +0900

    Update main.py
    Code cleanup

--- main.py
+++ main.py
@@ -6,23 +6,17 @@ import datetime
 from discord_webhook import DiscordWebhook
 
 import api
-from models import APIResult, GoalInfo
+from models import APIResult, GameStatus, GoalInfo
 from utils import get_team, timedelta_to_str
-
 
 last_launch = None
 
-async def change(data, old, new):
-    print("Game status changed: {old} -> {new}".format(old=old, new=new))
+async def change(data: APIResult, old: GameStatus, new: GameStatus):
+    pass
 
 async def goal(data: APIResult, goal_info: GoalInfo):
-    print(
-        f"{goal_info.player_name} of {goal_info.team} goal (assisted by {goal_info.assistted_player_name})\n" +
-        f"{goal_info.goal_type} - {goal_info.point_amount} points"
-    )
     webhook = DiscordWebhook(
-        url=config.WEBHOOK_URL, 
-        content=''
+        url=config.WEBHOOK_URL
     )
     embed = DiscordEmbed(title=f'ゴール！ - {goal_info.goal_type.name.replace("_", " ").title()}', description=f":{goal_info.team}_square: {goal_info.team.title()}チームに{goal_info.point_amount}ポイント！")
     embed.add_embed_field(name="シュートしたプレイヤー", value=goal_info.player_name, inline=False)
@@ -41,8 +35,7 @@ async def launch(data: APIResult):
 
 async def over(data: APIResult):
     webhook = DiscordWebhook(
-        url=config.WEBHOOK_URL, 
-        content=''
+        url=config.WEBHOOK_URL
     )
     beat_team = data.teams[int(data.blue_round_score < data.orange_round_score)]
     embed = DiscordEmbed(title='Round Over!', description=f"ラウンド終了、このラウンドは{beat_team.name.title()}チームの勝利です！")
@@ -60,14 +53,6 @@ async def end(data: APIResult):
     embed.add_embed_field(name=f"ラウンド数 {data.total_round_count}回", value=f":orange_square:オレンジチーム ラウンド勝利数: {data.orange_round_score}\n:blue_square:ブルーチーム ラウンド勝利数: {data.blue_round_score}", inline=False)
     webhook.add_embed(embed)
     webhook.execute()
-    beat_team = data.teams[int(data.blue_round_score < data.orange_round_score)]
-    client_team = get_team(data.teams, data.client_name)
-    print("{team} won - Orange: {orange}, Blue: {blue}".format(team=beat_team.name, orange=data.orange_round_score, blue=data.blue_round_score))
-    if beat_team == client_team:
-        print("Victory!")
-
-
-
 
 api.on_game_status_change = change
 api.on_goal = goal
@@ -76,7 +61,6 @@ api.on_round_end = over
 api.on_launch = launch
 
 def main():
-    
     loop = asyncio.get_event_loop()
     task = loop.create_task(api.run(host=config.HOST, port=config.PORT, endpoint=config.ENDPOINT, interval=config.INTERVAL))
     try:
